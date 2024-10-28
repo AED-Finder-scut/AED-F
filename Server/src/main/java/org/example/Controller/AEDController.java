aeafaca389a28a16218e8ae1e234598cf05ac41f
June0615o <1735419274@qq.com>
2024-10-28 13:05:43 +0800

    增加了返回附近AED时按距离排序的功能

--- Server/src/main/java/org/example/Controller/AEDController.java
+++ Server/src/main/java/org/example/Controller/AEDController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
@@ -19,27 +21,30 @@ public class AEDController {
     static {
         aeds.add(new AED(1L,"C17", "C17教师公寓一楼大厅",  23.057886, 113.408575));
         aeds.add(new AED(2L, "生活区操场","中国邮政旁/生活区运动场器材室门口",  23.056992, 113.408629));
-        aeds.add(new AED(3L,"校医院", "校医院门诊部一楼", 113.410232,23.058651));
-        aeds.add(new AED(4L, "C5","C5学生宿舍一楼大厅",  113.407973,23.054573));
-        aeds.add(new AED(5L,"D2", "D2教师公寓物业服务中心门口",  113.408984,23.053347));
-        aeds.add(new AED(6L,"教学区操场", "教学区操场架空厅",  113.409896,23.051589));
-        aeds.add(new AED(7L, "B6","B6生物中心后座一楼",  113.409896,23.051589));
-        aeds.add(new AED(8L,"正门", "正门东侧保安室",  113.415923,23.050445));
-        aeds.add(new AED(9L, "图书馆","图书馆二楼后门", 113.411858,23.052927));
-        aeds.add(new AED(10L, "A3","A3教学楼一楼大厅", 113.41193,23.055791));
-        aeds.add(new AED(11L, "B10","B10北座一楼",  113.4148,23.056298));
+        aeds.add(new AED(3L,"校医院", "校医院门诊部一楼", 23.058651,113.410232));
+        aeds.add(new AED(4L, "C5","C5学生宿舍一楼大厅",  23.054573, 113.407973));
+        aeds.add(new AED(5L,"D2", "D2教师公寓物业服务中心门口",  23.053347, 113.408984));
+        aeds.add(new AED(6L,"教学区操场", "教学区操场架空厅",  23.051589, 113.409896));
+        aeds.add(new AED(7L, "B6","B6生物中心后座一楼",  23.051589, 113.409896));
+        aeds.add(new AED(8L,"正门", "正门东侧保安室",  23.050445, 113.415923));
+        aeds.add(new AED(9L, "图书馆","图书馆二楼后门", 23.052927, 113.411858));
+        aeds.add(new AED(10L, "A3","A3教学楼一楼大厅", 23.055791, 113.41193));
+        aeds.add(new AED(11L, "B10","B10北座一楼",  23.056298, 113.4148));
     }
 
     @GetMapping("/nearby")
     public List<AED> getNearbyAeds(@RequestParam double lat, @RequestParam double lon, @RequestParam double radius) {
         List<AED> nearbyAeds = new ArrayList<>();
         for (AED aed : aeds) {
-            radius=0.5; //默认查找半径500米内.当且仅当距离小于半径的才会被添加至最终显示的AED列表nearbyAeds中.
+             //当且仅当距离小于半径的才会被添加至最终显示的AED列表nearbyAeds中.
             double distance = haversine(lat, lon, aed.getLatitude(), aed.getLongitude());
             if (distance <= radius) {
                 nearbyAeds.add(aed);
+                aed.setDistance(distance);
             }
         }
+        Collections.sort(nearbyAeds,Comparator.comparingDouble(AED::getDistance));
+        //会根据距离 由近及远地返回附近的AED
         return nearbyAeds;
     }
 
--- Server/src/main/java/org/example/entities/AED.java
+++ Server/src/main/java/org/example/entities/AED.java
@@ -6,6 +6,7 @@ public class AED {
     private String description;
     private double latitude; //经度
     private double longitude; //纬度
+    private double distance;
 
     public AED(Long id) {
         this.id = id;
@@ -59,6 +60,14 @@ public class AED {
         this.longitude = longitude;
     }
 
+    public double getDistance() {  //只有在涉及到用户具体位置时才能调用，因为他没有固定的默认值，否则报错.
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
     @Override
     public String toString() {
         return "AED{" +
@@ -67,8 +76,7 @@ public class AED {
                 ", description='" + description + '\'' +
                 ", latitude=" + latitude +
                 ", longitude=" + longitude +
+                ", distance=" + distance +
                 '}';
     }
-
-    // 构造函数、Getter和Setter以及用于测试的toString方法
 }
